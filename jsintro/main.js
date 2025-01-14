3a23ee8413e7fb766afcbdfbc8a2a544f48604c5
FidanUmudova <fidanumudova05@gmail.com>
2025-01-14 21:09:41 +0400

    Add files via upload

---
+++ jsintro/main.js
@@ -0,0 +1,13 @@
+let grade=35;
+if( grade > 90){
+    alert("Əlacı");
+}
+else if( grade>=70 && grade <= 90){
+    alert("Yaxsi");
+}
+else if( grade >= 50 && grade <= 70){
+    alert("Kafi");
+}
+else if(grade >= 0 && grade <= 50){
+    alert("Kesilir");
+}
